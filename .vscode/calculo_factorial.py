72726e2aeed4f63223f43fbb5bcab6f52dd72aa1
esteban2525 <126501993+esteban2525@users.noreply.github.com>
2024-08-31 15:43:54 +0000

    calculo factotial

--- .vscode/calculo_factorial.py
+++ .vscode/calculo_factorial.py
@@ -2,4 +2,13 @@ def calculoFactorial (num:int)->str|int :
     resultado : int
     if num < 0 :
         return "No se puede valores negativos" 
+    elif num == 0:
+          return 1
+    for n in range(1,num+1):
+        resultado = resultado * n
+    return resultado
+
+
+
+    
     
